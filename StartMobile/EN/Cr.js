9be7f0bee56955b47d36779fb2d42c173cba4a79
Berkerio4k <161024367+Berkerio@users.noreply.github.com>
2024-09-29 13:41:25 +0200

    FinALvERSION

--- StartMobile/EN/Cr.js
+++
@@ -1,6 +0,0 @@
-//assegno alle variabili HTML alle mie nuove js
-const videoBackground = document.querySelector('#video-background');
-
-videoBackground.addEventListener('ended', () => {
-  videoLoop.play(); // Avvia il secondo video in loop
-});
